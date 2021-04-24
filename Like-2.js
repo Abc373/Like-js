bd7026aed6da6004cda93af7aaf9f6fb6f97f8e6
Base_man <78816891+Abc373@users.noreply.github.com>
2021-04-24 19:42:53 +0800

    Update Like-2.js

--- Like-2.js
+++ Like-2.js
@@ -26,6 +26,21 @@ var Like = function(like_finger, like_number, on_color, leave_color) {
 let information = {
     "version": "v0.0.1",
     "copyright": "Base_man",
-    "How to use the method": "In 'like-1.js'",
-    "What did I update": "Remove all IDs and colors to parameters you can pass"
+    "What did I update": "Remove all IDs and colors to parameters you can pass",
+    "How to invoke the function":{
+        "Example(HTML invoke)":{
+            "line1":"<!DOCTYPE html>",
+            "line2":"<html lang='en'>",
+            "line3":"<head>",
+            "line4":"   <meta charset='utf-8'>",
+            "line5":"   <meta name='author' content='Base_man'>",
+            "line6":"   <title>Invoke JavaScript function",
+            "line7":"</head>",
+            "line8":"<body style="background-image: url(&quot;https://i0.hdslb.com/bfs/live/d4fab7a666bc627cc2bcb4f00aa6d2a7958cee80.png@1365w.webp&quot;);">",
+            "line9":"   <div id='like_finger'></div>",
+            "line10":"  <div id='like_finger'>233</div>",
+            "line11":"   <script type='text/javascript' src='[driver]:[path]\Like-2.js'></script>",
+            "line12":"  <script>Like("like_finger","like_number","blue","gray");</script>",
+            "line13":"</body>"
+        }
 }
